207fb1423cfa0f8b32f024fec60fa63cb60ad420
Shullski <mshull2@kent.edu>
2017-09-25 00:06:46 -0400

    Add files via upload

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,12 +1,42 @@
+var fireworks = [];
+var gravity;
+var canvas;
+var startDrawing = true;
+
 function setup() {
-  createCanvas(640, 480);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index', '-1');
+  colorMode(HSB);
+
+
+
+  //Set gravity
+  gravity = createVector(0, 0.15);
+  stroke(255);
+  strokeWeight(4);
+  background(255);
+  firework = new Particle(random(width), height);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  var fps = frameRate();
+  if (fps < 40) console.log(fps);
+
+  colorMode(RGB);
+  background(30,50);
+
+  if(startDrawing) {
+
+  if(random(0,1) > 0.80){
+    fireworks.push(new Firework());
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  for(var i = fireworks.length - 1; i >= 0; i--) {
+    fireworks[i].update();
+    fireworks[i].show();
+    if (fireworks[i].done()) {
+      fireworks.splice(i, 1);
+    }
+  }
+}
 }
